60e3710305a776550dd4f2c76c3dd6f22ec1f155
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2024-02-17 20:05:51 -0500

    Split Dao and Task from Day3

---
+++ src/Dao.java
@@ -0,0 +1,53 @@
+public class Dao {
+    private final static Task[] taskLists = new Task[5];
+
+    public Boolean addTask(Task task) {
+        for (int i = 0; i < taskLists.length; i++) {
+            if(taskLists[i]==null){
+                taskLists[i] = task;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean updateTask(int taskId, String newTitle, String newText) {
+        for (Task task : taskLists) {
+            if (task.getTaskId() == taskId) {
+                task.setTaskTitle(newTitle);
+                task.setTaskText(newText);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean deleteTask(int taskId) {
+        if(taskId>=0 && taskId<taskLists.length){
+            taskLists[taskId]=null;
+            return true;
+        }
+        return false;
+    }
+
+    public Task searchTask(int taskId) {
+        if(taskId>=0 && taskId<taskLists.length) {
+            return taskLists[taskId];
+        }
+        return null;
+    }
+
+    public boolean updateAssignedTo(int taskId, String assignedTo) {
+        for (Task task : taskLists) {
+            if (task.getTaskId() == taskId) {
+                task.setAssignedTo(assignedTo);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Task[] getTasks() {
+        return taskLists;
+    }
+}
---
+++ src/Task.java
@@ -0,0 +1,51 @@
+public class Task {
+    private static int lastTaskId = 0;
+    private final int taskId;
+    private String taskTitle;
+    private String taskText;
+    private String assignedTo;
+
+    public Task(String taskTitle, String taskText, String assignedTo) {
+        this.taskId = ++lastTaskId;
+        this.taskTitle = taskTitle;
+        this.taskText = taskText;
+        this.assignedTo = assignedTo;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public String getTaskTitle() {
+        return taskTitle;
+    }
+
+    public String getTaskText() {
+        return taskText;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+    public void setTaskTitle(String newTitle) {
+        this.taskTitle = newTitle;
+    }
+
+    public void setTaskText(String newText) {
+        this.taskText = newText;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskId=" + getTaskId() +
+                ", taskTitle='" + getTaskTitle() + '\'' +
+                ", taskText='" + getTaskText() + '\'' +
+                ", assignedTo='" + getAssignedTo() + '\'' +
+                '}';
+    }
+}
